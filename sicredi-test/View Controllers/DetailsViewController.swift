984f6201fc59bf65673eea56f2e6456e3c2c9833
Fernando Marins <fernandomarins@Fernandos-MacBook-Pro.local>
2021-12-15 18:24:56 -0300

    added two view controllers and configured them to display the data returned from the API

---
+++ sicredi-test/View Controllers/DetailsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailsViewController.swift
+//  sicredi-test
+//
+//  Created by Fernando Marins on 15/12/21.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ sicredi-test/View Controllers/EventsListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  EventsListViewController.swift
+//  sicredi-test
+//
+//  Created by Fernando Marins on 15/12/21.
+//
+
+import UIKit
+
+class EventsListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+}
+
+extension EventsListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)
+        
+        return cell
+    }
+    
+}
