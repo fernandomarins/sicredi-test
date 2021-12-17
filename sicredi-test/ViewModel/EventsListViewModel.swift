7fbec7562fa0bb38948554f41e8009817d4b9448
Fernando Marins <fernandomarins@Fernandos-MacBook-Pro.local>
2021-12-17 18:19:53 -0300

    finished eventlistviewmodel

--- sicredi-test/ViewModel/EventsListViewModel.swift
+++ sicredi-test/ViewModel/EventsListViewModel.swift
@@ -27,7 +27,7 @@ class EventsListViewModel {
     func getEvents() {
         eventService.getEventsService { success, results, error in
             if success, let events = results {
-//                self.fetchData()
+                self.fetchData(events: events)
             }
         }
     }
@@ -36,8 +36,27 @@ class EventsListViewModel {
         self.events = events
         var vms = [EventCellViewModel]()
         for event in events {
-//            vms.append(<#T##newElement: EventCellViewModel##EventCellViewModel#>)
+            vms.append(createCellModel(event: event))
         }
+        
+        eventCellViewModels = vms
+    }
+    
+    func createCellModel(event: Event) -> EventCellViewModel {
+        let description = event.description
+        let image = event.image
+        let longitude = event.longitude
+        let latitude = event.latitude
+        let price = event.price
+        let title = event.title
+        let id = event.id
+        let date = event.date
+        
+        return EventCellViewModel(description: description, image: image, longitude: longitude, latitude: latitude, price: price, title: title, id: id, date: date)
+    }
+    
+    func getCellViewModel(at indexPath: IndexPath) -> EventCellViewModel {
+        return eventCellViewModels[indexPath.row]
     }
 
     
