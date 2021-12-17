0584207ad0164b02f531ca995359c275b1505de4
Fernando Marins <fernandomarins@Fernandos-MacBook-Pro.local>
2021-12-17 16:26:31 -0300

    created the table view cell

---
+++ sicredi-test/View/Cell/EventCell.swift
@@ -0,0 +1,23 @@
+//
+//  EventCell.swift
+//  sicredi-test
+//
+//  Created by Fernando Marins on 17/12/21.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
